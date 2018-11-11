5ad523115069153782c65fcafb808dd5c0edb23b
Lorenzo <lorenzo@tilkee.com>
2018-11-11 23:46:09 +0100

    change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181111195722) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.string "artist"
